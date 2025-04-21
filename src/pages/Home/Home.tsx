cb8a025cfbd472fc61227c26e6c1b20facb3d7b2
Dare zayan. <143078072+darezyan@users.noreply.github.com>
2025-04-21 22:30:54 +0100

    Update Home.tsx

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,14 +1,12 @@
-import React from 'react'
 import Hero from '../../components/hero/Hero'
-import Features from '../../components/features/AboutUs'
 import Services from '../../components/Services/Services'
 import Contact from '../../components/Contact/Contact'
 
 
 
-type Props = {}
 
-const Home = (props: Props) => {
+
+const Home = () => {
   return (
    <div>
      <Hero />
